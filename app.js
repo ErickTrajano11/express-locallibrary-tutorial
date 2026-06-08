7f5c06b2e9d283a2e7fad1520b639fcb885188f4
Erick Inacio Trajano <erickinaciot@gmail.com>
2026-06-08 19:08:30 -0300

    Debugando o railway

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ const helmet = require("helmet");
 
 const app = express();
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 // Set up rate limiter: maximum of twenty requests per minute
 const limiter = RateLimit({
   windowMs: 1 * 60 * 1000, // 10 seconds
